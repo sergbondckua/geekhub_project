f516e724f16e6023839a1e7bf192004eb3be1ec0
sergbondckua <sergninja@gmail.com>
2023-04-15 17:10:02 +0300

    bug text fix

--- profiles/apps.py
+++ profiles/apps.py
@@ -4,4 +4,4 @@ from django.utils.translation import gettext_lazy as _
 class ProfilesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "profiles"
-    verbose_name = _("Профілі")
+    verbose_name = _("Profiles")
--- sportevent/apps.py
+++ sportevent/apps.py
@@ -5,7 +5,7 @@ from django.utils.translation import gettext_lazy as _
 class SporteventConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "sportevent"
-    verbose_name = _("Спортивні заходи")
+    verbose_name = _("Sports events")
 
     def ready(self):
         import sportevent.signals
